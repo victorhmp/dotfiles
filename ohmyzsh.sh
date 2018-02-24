3900c527636017ac2930867910d588deed0380f0
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-02-24 01:42:33 -0300

    Unification of Zsh configuration
    `ohmyzsh.sh` instals zsh and selects it as the default shell.
    
    After that, it instals oh-my-zsh framework and PowerLevel9K theme for
    zsh.

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Install Zsh
+brew install zsh zsh-completions
+
+# Make Zsh default shell
+sudo sh -c "echo $(which zsh) >> /etc/shells"
+chsh -s $(which zsh)
+
+# Install Oh-My-Zsh framework
 rm -rf ~/.oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# Install Powerlevel9K theme to Zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
