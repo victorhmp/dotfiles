73aa5db53b33233a13599b4350d6f9a8f66da76b
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-06-21 16:52:27 -0300

    Delete old files that are now unnecessary

--- iTerm2-config/setup-italics.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This simple scripts takes a .txt file and do some magic to make terminal
-# italics work!
-
-tic -o ~/.terminfo xterm-256color.terminfo.txt
--- install-vundle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script just installs Vundle plugin manager for Vim
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
