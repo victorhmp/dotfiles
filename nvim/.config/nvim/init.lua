1a406e840a0ef236acfb9d18f0df588e108e20a1
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-10-06 20:12:42 -0300

    Add linting and formatting rules for typescript and json

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -450,6 +450,8 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
       },
     },
   },
